b762b9261b75a4333dd7b36020be94b0a2d9d66a
fow07bx99kt0 <felipers0799@gmail.com>
2025-07-18 15:48:53 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ function loadForm(type) {
   const formContainer = document.getElementById("form-container");
   let url = "";
   if (type === "viagem") {
-    url = "https://forms.office.com/pages/responsepage.aspx?id=emONPyFz50qudewosdtpDEK_Qs0Wl59OqI5w8rrHBSlUNDcxRlZHRzBKMjVJM1lWVFc4UU8zOUc0SS4u&origin=lprLink&route=shorturl";
-  } else if (type === "hospedagem") {
     url = "https://forms.office.com/pages/responsepage.aspx?id=emONPyFz50qudewosdtpDEK_Qs0Wl59OqI5w8rrHBSlUNjZYRUdHUEtKNUNDSEYyV001TE9VU0ZUVy4u&origin=lprLink&route=shorturl";
+  } else if (type === "hospedagem") {
+    url = "https://forms.office.com/pages/responsepage.aspx?id=emONPyFz50qudewosdtpDEK_Qs0Wl59OqI5w8rrHBSlUNDcxRlZHRzBKMjVJM1lWVFc4UU8zOUc0SS4u&origin=lprLink&route=shorturl";
   } else if (type === "inovacao") {
     url = "https://forms.office.com/Pages/ResponsePage.aspx?id=emONPyFz50qudewosdtpDCPJHkcW2CpMmyPiHvcb7dFUOUxBVkkzTExFVlZSWVNETjVSMkw5MDZJRi4u";
   }
