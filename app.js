e44e60fd8c1e27091edb22f574abc8fed5000852
fow07bx99kt0 <felipers0799@gmail.com>
2025-07-21 11:05:27 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,6 @@
 
-function loadForm(type) {
-  const icons = document.getElementById("icons");
-  const formContainer = document.getElementById("form-container");
-  const iframe = document.getElementById("form-frame");
-  let url = "";
-  if (type === "viagem") {
-    url = "https://forms.office.com/pages/responsepage.aspx?id=emONPyFz50qudewosdtpDEK_Qs0Wl59OqI5w8rrHBSlUNjZYRUdHUEtKNUNDSEYyV001TE9VU0ZUVy4u&origin=lprLink&route=shorturl";
-  } else if (type === "hospedagem") {
-    url = "https://forms.office.com/pages/responsepage.aspx?id=emONPyFz50qudewosdtpDEK_Qs0Wl59OqI5w8rrHBSlUNDcxRlZHRzBKMjVJM1lWVFc4UU8zOUc0SS4u&origin=lprLink&route=shorturl";
-  } else if (type === "inovacao") {
-    url = "https://forms.office.com/Pages/ResponsePage.aspx?id=emONPyFz50qudewosdtpDCPJHkcW2CpMmyPiHvcb7dFUOUxBVkkzTExFVlZSWVNETjVSMkw5MDZJRi4u";
-  }
-  icons.style.display = "none";
-  formContainer.style.display = "block";
-  iframe.src = url;
-}
-
-function voltarInicio() {
-  document.getElementById("form-frame").src = "";
-  document.getElementById("form-container").style.display = "none";
-  document.getElementById("icons").style.display = "flex";
-}
-
-function mostrarSenha() {
-  document.getElementById("senha-container").style.display = "block";
-}
-
-function validarSenha() {
-  const senha = document.getElementById("senhaInput").value;
-  if (senha === "corpal2025") {
-    const icons = document.getElementById("icons");
-    const formContainer = document.getElementById("form-container");
-    const iframe = document.getElementById("form-frame");
-
-    icons.style.display = "none";
-    formContainer.style.display = "block";
-    iframe.src = "painel.html";
-  } else {
-    alert("Senha incorreta.");
-  }
+function mostrarPainel(tipo) {
+  const paineis = document.querySelectorAll('.painel');
+  paineis.forEach(p => p.style.display = 'none');
+  document.getElementById(tipo).style.display = 'block';
 }
