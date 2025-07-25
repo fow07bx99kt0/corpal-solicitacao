3af7b0f741ae95f3c2dfcab7829836e6ccdc5e94
fow07bx99kt0 <felipers0799@gmail.com>
2025-07-25 08:28:07 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ function loadForm(type) {
   const formContainer = document.getElementById("form-container");
   const iframe = document.getElementById("form-frame");
   let url = "";
+
   if (type === "viagem") {
     url = "https://forms.office.com/pages/responsepage.aspx?id=emONPyFz50qudewosdtpDEK_Qs0Wl59OqI5w8rrHBSlUNjZYRUdHUEtKNUNDSEYyV001TE9VU0ZUVy4u&origin=lprLink&route=shorturl";
   } else if (type === "hospedagem") {
     url = "https://forms.office.com/pages/responsepage.aspx?id=emONPyFz50qudewosdtpDEK_Qs0Wl59OqI5w8rrHBSlUNDcxRlZHRzBKMjVJM1lWVFc4UU8zOUc0SS4u&origin=lprLink&route=shorturl";
   } else if (type === "inovacao") {
     url = "https://forms.office.com/Pages/ResponsePage.aspx?id=emONPyFz50qudewosdtpDCPJHkcW2CpMmyPiHvcb7dFUOUxBVkkzTExFVlZSWVNETjVSMkw5MDZJRi4u";
+  } else if (type === "cessao") {
+    url = "https://form.jotform.com/252023929762056";
   }
+
   icons.style.display = "none";
   formContainer.style.display = "block";
   iframe.src = url;
